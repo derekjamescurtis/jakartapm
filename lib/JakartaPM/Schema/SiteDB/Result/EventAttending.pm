f8ee3000445e3606b37efd7eecee1581e5667813
Derek J. Curtis <djcurtis@summersetsoftware.com>
2013-10-04 10:14:32 +0700

    Removed auto-regeneration from model classes and formatted for readability.  Removed non-minified css/js files from project.

--- lib/JakartaPM/Schema/SiteDB/Result/EventAttending.pm
+++ lib/JakartaPM/Schema/SiteDB/Result/EventAttending.pm
@@ -1,22 +1,14 @@
-use utf8;
 package JakartaPM::Schema::SiteDB::Result::EventAttending;
-
-# Created by DBIx::Class::Schema::Loader
-# DO NOT MODIFY THE FIRST PART OF THIS FILE
-
-=head1 NAME
-
-JakartaPM::Schema::SiteDB::Result::EventAttending
-
-=cut
-
-use strict;
-use warnings;
-
+use Modern::Perl '2010';
 use Moose;
 use MooseX::NonMoose;
 use MooseX::MarkAsMethods autoclean => 1;
 extends 'DBIx::Class::Core';
+
+
+=head1 NAME
+
+JakartaPM::Schema::SiteDB::Result::EventAttending
 
 =head1 COMPONENTS LOADED
 
@@ -106,10 +98,5 @@ __PACKAGE__->belongs_to(
 );
 
 
-# Created by DBIx::Class::Schema::Loader v0.07036 @ 2013-09-12 12:24:56
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:K1uYnTBXta+nzK1aFjerbQ
-
-
-# You can replace this text with custom code or comments, and it will be preserved on regeneration
 __PACKAGE__->meta->make_immutable;
 1;
